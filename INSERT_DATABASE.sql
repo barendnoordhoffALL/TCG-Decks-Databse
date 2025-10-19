ae01991f224f10e8de96d5aa2750db6b1065f52f
Barend Noordhoff <barend.noordhoff@alliander.com>
2025-10-19 17:16:27 +0200

    Removed custom data sql file, felt it was not nessecary

--- INSERT_DATABASE.sql
+++ INSERT_DATABASE.sql
@@ -35,3 +35,26 @@ insert into status (status) values ('Build'),
                                    ('Interested'),
                                    ('Not in this meta'),
                                    ('Taken apart');
+
+
+
+insert into onepiecedeck (leader, status, set, power, life) values
+                                ('Luffy', 'Not in this meta', 'OP05', 5000, 5),
+                                ('Reiju', 'Build', 'OP06', 5000, 4),
+                                ('Gecko Moria', 'Building', 'OP06', 5000, 5),
+                                ('Lucci', 'Interested', 'OP07', 5000, 5),
+                                ('Kalgara', 'Taken apart', 'OP08', 5000, 5),
+                                ('Lim', 'Taken apart', 'OP09', 5000, 4),
+                                ('Buggy', 'Build', 'OP09', 5000, 5),
+                                ('Luffy', 'Build', 'OP09', 5000, 4),
+                                ('Blackbeard', 'Build', 'OP09', 5000, 5),
+                                ('Law', 'Building', 'OP10', 5000, 4),
+                                ('Luffy', 'Build', 'OP11', 6000, 3),
+                                ('Kuzan', 'Build', 'OP12', 5000, 5),
+                                ('Zoro', 'Interested', 'OP12', 5000, 5),
+                                ('Sabo', 'Building', 'OP13', 5000, 5),
+                                ('Luffy', 'Taken apart', 'ST08', 5000, 5),
+                                ('Zoro & Sanji', 'Interested', 'ST12', 5000, 4),
+                                ('Uta', 'Build', 'ST16', 5000, 5),
+                                ('Smoker', 'Taken apart', 'ST18', 5000, 5),
+                                ('Ace & Whitebeard', 'Build', 'ST22', 5000, 5)
